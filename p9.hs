70ade371b42347792604c74695cab8128009ba44
Lincoln Quirk <lincolnq@gmail.com>
2022-12-09 09:00:00 -0500

    solve part2

--- p9.hs
+++ p9.hs
@@ -12,42 +12,42 @@ test fn = testInp <&> fn
 
 parsed = map (first head . second readInt . tup2 . words) . lines
 
-type S = ((Int, Int), (Int, Int))
+type S = ((Int, Int), [(Int, Int)])
 
 type Visited = S.Set (Int, Int)
 
-moveHead :: Char -> S -> S
-moveHead 'R' ((x,y), t) = ((x+1, y),t)
-moveHead 'L' ((x,y), t) = ((x-1, y),t)
-moveHead 'U' ((x,y), t) = ((x, y-1),t)
-moveHead 'D' ((x,y), t) = ((x, y+1),t)
+moveHead :: Char -> (Int, Int) -> (Int, Int)
+moveHead 'R' (x,y) = (x+1, y)
+moveHead 'L' (x,y) = (x-1, y)
+moveHead 'U' (x,y) = (x, y-1)
+moveHead 'D' (x,y) = (x, y+1)
 
 
 
-moveTail :: S -> S
+moveFollower :: ((Int,Int),(Int,Int)) -> (Int,Int)
 -- no move in y if close
-moveTail d@((x,y),(tx,ty)) | x==tx, abs(y-ty) < 2 = d
+moveFollower ((x,y),d@(tx,ty)) | x==tx, abs(y-ty) < 2 = d
 -- no move in x if close
-moveTail d@((x,y),(tx,ty)) | y==ty, abs(x-tx) < 2 = d
+moveFollower ((x,y),d@(tx,ty)) | y==ty, abs(x-tx) < 2 = d
 
 -- move in y if further
-moveTail (h@(x,y),(tx,ty)) | x==tx, y>ty = (h, (tx,ty+1))
-moveTail (h@(x,y),(tx,ty)) | x==tx, y<ty = (h, (tx,ty-1))
+moveFollower (h@(x,y),(tx,ty)) | x==tx, y>ty = (tx,ty+1)
+moveFollower (h@(x,y),(tx,ty)) | x==tx, y<ty = (tx,ty-1)
 
 -- move in x if further
-moveTail (h@(x,y),(tx,ty)) | y==ty, x>tx = (h, (tx+1,ty))
-moveTail (h@(x,y),(tx,ty)) | y==ty, x<tx = (h, (tx-1,ty))
+moveFollower (h@(x,y),(tx,ty)) | y==ty, x>tx = (tx+1,ty)
+moveFollower (h@(x,y),(tx,ty)) | y==ty, x<tx = (tx-1,ty)
 
 -- close diagonals
-moveTail d@((x,y),(tx,ty)) | abs(y-ty)<2, abs(x-tx) < 2 = d
+moveFollower ((x,y),d@(tx,ty)) | abs(y-ty)<2, abs(x-tx) < 2 = d
 
 -- farther diagonals
-moveTail (h@(x,y),(tx,ty)) | y>ty, x>tx = (h, (tx+1,ty+1))
-moveTail (h@(x,y),(tx,ty)) | y<ty, x>tx = (h, (tx+1,ty-1))
-moveTail (h@(x,y),(tx,ty)) | y>ty, x<tx = (h, (tx-1,ty+1))
-moveTail (h@(x,y),(tx,ty)) | y<ty, x<tx = (h, (tx-1,ty-1))
+moveFollower (h@(x,y),(tx,ty)) | y>ty, x>tx = (tx+1,ty+1)
+moveFollower (h@(x,y),(tx,ty)) | y<ty, x>tx = (tx+1,ty-1)
+moveFollower (h@(x,y),(tx,ty)) | y>ty, x<tx = (tx-1,ty+1)
+moveFollower (h@(x,y),(tx,ty)) | y<ty, x<tx = (tx-1,ty-1)
 
-moveTail d = d
+moveFollower (_,d) = d
 
 step :: S -> (Char, Int) -> State Visited S
 step s0 (c, 0) = return s0
@@ -55,12 +55,18 @@ step s0 (c, n) = do
     s1 <- step1 c s0
     step s1 (c, n-1) 
 
-step1 c s0 = do
+step1 c (h, ts) = do
     
-    let s1 = moveHead c s0
-    let s2 = moveTail s1
-    
-    modify $ S.insert (snd s2)
-    return $ trace (show s2) s2
+    let nh = moveHead c h
 
-part1 = length . (\p -> execState (foldM_ step ((0,0),(0,0)) p) S.empty) . parsed
+    (_, ts') <- foldM (\(ref, ts') f -> do
+        let f' = moveFollower (ref,f)
+        return $ (f',ts' ++ [f'])
+        ) (nh,[]) ts
+--    let f = moveFollower (nh, t)
+    
+    modify $ S.insert (last ts')
+    return $ trace (show (nh, ts')) (nh, ts')
+
+part1 = length . (\p -> execState (foldM_ step ((0,0),[(0,0)]) p) S.empty) . parsed
+part2 = length . (\p -> execState (foldM_ step ((0,0),replicate 9 (0,0)) p) S.empty) . parsed
